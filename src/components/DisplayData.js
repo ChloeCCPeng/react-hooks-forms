fa98f689413352131fdeef58e01280606c1f320c
Chloe <chloepeng331@icloud.com>
2022-02-27 10:43:12 -0500

    commit tree 81a58abfde54c8d9fe324698f9422eb2090e561f

--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 
 function DisplayData({firstName, lastName}) {
+    // console.log("hello")
   return (
     <div>
         <h1>{firstName}</h1>
         <h1>{lastName}</h1>
+        
     </div>
-  )
+  );
 }
 
 export default DisplayData
